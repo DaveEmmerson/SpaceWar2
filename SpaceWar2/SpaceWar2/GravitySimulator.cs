5ec64bbb12c0f78613e2675e1a2ec6d792d7764d
Matthew Watson <mattwatson1@gmail.com>
2012-09-30 00:46:00 +0100

    Added GravitySimulator
    Objects will be registered with the gravitySimulator and it will deal out gravity from and to them

---
+++ SpaceWar2/SpaceWar2/GravitySimulator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceWar2
+{
+    public class GravitySimulator
+    {
+        private readonly IList<IGameObject> _sources = new List<IGameObject>();
+        private readonly IList<IGameObject> _participants = new List<IGameObject>();
+
+        internal void RegisterSource(IGameObject source)
+        {
+            _sources.Add(source);
+        }
+
+        internal void RegisterParticipant(IGameObject participant)
+        {
+            _participants.Add(participant);
+        }
+
+        internal void Simulate()
+        {
+            foreach (var source in _sources)
+            {
+                foreach (var participant in _participants)
+                {
+                    var gravity = CalculateGravity
+                    participant.DoSoemthing
+                }
+            }
+        }
+    }
+}
