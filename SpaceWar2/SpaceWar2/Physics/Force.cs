ebab681d188149a2fd215047e9c84e91e34eaf21
David Emmerson <david.emmerson@gmail.com>
2012-10-23 00:49:20 +0100

    Added unit tests project. Amended Force to be public.

--- SpaceWar2/SpaceWar2/Physics/Force.cs
+++ SpaceWar2/SpaceWar2/Physics/Force.cs
@@ -2,27 +2,27 @@
 
 namespace DEMW.SpaceWar2.Physics
 {
-    internal class Force
+    public class Force
     {
-        internal Force() : this(Vector2.Zero, Vector2.Zero) { }
+        public Force() : this(Vector2.Zero, Vector2.Zero) { }
         
-        internal Force(Vector2 force) : this(force, Vector2.Zero) { }
+        public Force(Vector2 force) : this(force, Vector2.Zero) { }
 
-        internal Force(Vector2 force, Vector2 displacement)
+        public Force(Vector2 force, Vector2 displacement)
         {
             Vector = force;
             Displacement = displacement;
         }
 
-        internal Vector2 Vector { get; private set; }
-        internal Vector2 Displacement { get; private set; }
+        public Vector2 Vector { get; private set; }
+        public Vector2 Displacement { get; private set; }
 
         public static Force operator + (Force f1, Force f2)
         {
             return new Force(f1.Vector + f2.Vector, f1.Displacement + f2.Displacement);
         }
 
-        internal void Rotate(float angle)
+        public void Rotate(float angle)
         {
             var rotation = Matrix.CreateRotationZ(angle);
             Vector = Vector2.Transform(Vector, rotation);
---
+++ SpaceWar2Tests/Physics/ForceTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using DEMW.SpaceWar2.Physics;
+using Microsoft.Xna.Framework;
+
+namespace DEMW.SpaceWar2Tests.Physics
+{
+    [TestFixture]
+    class ForceTests
+    {
+        private Force _dispRightVectorUp;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dispRightVectorUp = new Force(new Vector2(1, 0), new Vector2(0, -1));
+        }
+
+        [Test]
+        public void Offset_Force_Added_To_Self_Gives_Same_Displacement_And_Double_The_Vector()
+        {
+            var result = _dispRightVectorUp + _dispRightVectorUp;
+
+            Assert.AreEqual(_dispRightVectorUp.Displacement, result.Displacement);
+
+            Assert.AreEqual(_dispRightVectorUp.Vector * 2, result.Vector);
+
+        }
+    }
+}
