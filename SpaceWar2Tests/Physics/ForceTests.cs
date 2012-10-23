e7a10e96fb42d2ac513f88b700bf1269bb489c1b
David Emmerson <david.emmerson@gmail.com>
2012-10-23 01:33:48 +0100

    Added a second test on Force. Centre force doubled up. This commit also appears to have picked up some of the directory changes.

--- SpaceWar2Tests/Physics/ForceTests.cs
+++ SpaceWar2Tests/Physics/ForceTests.cs
@@ -8,11 +8,13 @@ namespace DEMW.SpaceWar2Tests.Physics
     class ForceTests
     {
         private Force _dispRightVectorUp;
+        private Force _noDispVectorRight;
 
         [SetUp]
         public void Setup()
         {
-            _dispRightVectorUp = new Force(new Vector2(1, 0), new Vector2(0, -1));
+            _dispRightVectorUp = new Force(displacement:new Vector2(1, 0), force:new Vector2(0, -1));
+            _noDispVectorRight = new Force(displacement:new Vector2(0, 0), force:new Vector2(1, 0));
         }
 
         [Test]
@@ -20,10 +22,20 @@ namespace DEMW.SpaceWar2Tests.Physics
         {
             var result = _dispRightVectorUp + _dispRightVectorUp;
 
-            Assert.AreEqual(_dispRightVectorUp.Displacement, result.Displacement);
+            Assert.AreEqual(_dispRightVectorUp.Displacement, result.Displacement, "Displacement");
 
-            Assert.AreEqual(_dispRightVectorUp.Vector * 2, result.Vector);
+            Assert.AreEqual(_dispRightVectorUp.Vector * 2, result.Vector, "Vector");
 
+        }
+
+        [Test]
+        public void Centre_Force_Added_To_Self_Gives_Zero_Displacement_And_Double_The_Vector()
+        {
+            var result = _noDispVectorRight + _noDispVectorRight;
+
+            Assert.AreEqual(Vector2.Zero, result.Displacement, "Displacement");
+
+            Assert.AreEqual(_noDispVectorRight.Vector * 2, result.Vector, "Vector");
         }
     }
 }
