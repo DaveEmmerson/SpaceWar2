a893d417a3d80a2eb13f80f7272cf03f911ded61
David Emmerson <david.emmerson@gmail.com>
2012-10-24 23:56:00 +0100

    Added a few more tests to Force.

--- SpaceWar2Tests/Physics/ForceTests.cs
+++ SpaceWar2Tests/Physics/ForceTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using DEMW.SpaceWar2.Physics;
 using Microsoft.Xna.Framework;
+using System;
 
 namespace DEMW.SpaceWar2Tests.Physics
 {
@@ -9,12 +10,20 @@ namespace DEMW.SpaceWar2Tests.Physics
     {
         private Force _dispRightVectorUp;
         private Force _noDispVectorRight;
+        private Force _dispDiagonalRightDownVectorDiagonalUpRight;
 
         [SetUp]
         public void Setup()
         {
-            _dispRightVectorUp = new Force(displacement:new Vector2(1, 0), force:new Vector2(0, -1));
-            _noDispVectorRight = new Force(displacement:new Vector2(0, 0), force:new Vector2(1, 0));
+            var right = new Vector2(1,0);
+            var up = new Vector2(0,-1);
+            var root2over2 = (float)Math.Sqrt(2) / 2f;
+            var downRight = new Vector2(root2over2, root2over2);
+            var upRight = new Vector2(root2over2, -root2over2);
+            
+            _dispRightVectorUp = new Force(right, up);
+            _noDispVectorRight = new Force(right, Vector2.Zero);
+            _dispDiagonalRightDownVectorDiagonalUpRight = new Force(upRight, downRight);
         }
 
         [Test]
@@ -37,5 +46,43 @@ namespace DEMW.SpaceWar2Tests.Physics
 
             Assert.AreEqual(_noDispVectorRight.Vector * 2, result.Vector, "Vector");
         }
+
+        [Test]
+        public void Offset_Force_Added_To_Opposite_Offset_Force_Gives_What()
+        {
+            Assert.Fail("I think there is a problem here. How can you combine these two forces?");
+
+        }
+
+        [Test]
+        public void Rotate_By_Small_Angle_Does_Not_Affect_Zero_Displacement()
+        {
+
+            _noDispVectorRight.Rotate(MathHelper.ToRadians(5));
+
+            Assert.AreEqual(Vector2.Zero, _noDispVectorRight.Displacement);
+
+        }
+
+        [Test]
+        public void Rotate_By_Large_Angle_Does_Not_Affect_Zero_Displacement()
+        {
+
+            _noDispVectorRight.Rotate(MathHelper.ToRadians(300));
+
+            Assert.AreEqual(Vector2.Zero, _noDispVectorRight.Displacement);
+
+        }
+
+        [Test]
+        public void Rotate_Calculates_Displacement_And_Vector_Correctly()
+        {
+            _dispRightVectorUp.Rotate(MathHelper.ToRadians(45));
+
+            Assert.AreEqual(_dispDiagonalRightDownVectorDiagonalUpRight.Displacement, _dispRightVectorUp.Displacement, "Displacement");
+            Assert.AreEqual(_dispDiagonalRightDownVectorDiagonalUpRight.Vector, _dispRightVectorUp.Vector, "Vector");
+
+        }
+
     }
 }
