02fe878d41942c70fcd67f66cd109a30dbebc5d6
Matt Watson <mattwatson1@gmail.com>
2012-11-01 00:04:21 +0000

    Added more ThrusterArrayTests

--- SpaceWar2Tests/GameObjects/ShipComponents/ThrusterArrayTests.cs
+++ SpaceWar2Tests/GameObjects/ShipComponents/ThrusterArrayTests.cs
@@ -13,6 +13,8 @@ namespace DEMW.SpaceWar2Tests.GameObjects.ShipComponents
     {
         private IShip _ship;
         private ThrusterArray _thrusterArray;
+        private Vector2 _leftThrusterPosition;
+        private Vector2 _rightThrusterPosition;
 
         [SetUp]
         public void Setup()
@@ -21,6 +23,9 @@ namespace DEMW.SpaceWar2Tests.GameObjects.ShipComponents
             _ship.Radius.Returns(10);
             _ship.AngularVelocity.Returns(0);
             _thrusterArray = new ThrusterArray(_ship);
+
+            _leftThrusterPosition = new Vector2(-_ship.Radius, 0f);
+            _rightThrusterPosition = new Vector2(_ship.Radius, 0f);
         }
 
         [Test]
@@ -59,16 +64,14 @@ namespace DEMW.SpaceWar2Tests.GameObjects.ShipComponents
             _thrusterArray.CalculateThrustPattern(ShipAction.Thrust);
             _thrusterArray.EngageThrusters();
 
-            var leftThrusterPosition = new Vector2(-_ship.Radius, 0f);
-            var rightThrusterPosition = new Vector2(_ship.Radius, 0f);
             var forwardVector = new Vector2(0f, -ThrusterArray.ThrustPower);
 
             _ship.Received().AngularVelocity = 0f;
             _ship.Received(2).ApplyInternalForce(Arg.Any<Force>());
             _ship.Received().ApplyInternalForce(
-                Arg.Is<Force>(x => x.Displacement == leftThrusterPosition && x.Vector == forwardVector));
+                Arg.Is<Force>(x => x.Displacement == _leftThrusterPosition && x.Vector == forwardVector));
             _ship.Received().ApplyInternalForce(
-                Arg.Is<Force>(x => x.Displacement == rightThrusterPosition && x.Vector == forwardVector));
+                Arg.Is<Force>(x => x.Displacement == _rightThrusterPosition && x.Vector == forwardVector));
         }
 
         [Test]
@@ -78,21 +81,78 @@ namespace DEMW.SpaceWar2Tests.GameObjects.ShipComponents
             _thrusterArray.CalculateThrustPattern(ShipAction.ReverseThrust);
             _thrusterArray.EngageThrusters();
 
-            var leftThrusterPosition = new Vector2(-_ship.Radius, 0f);
-            var rightThrusterPosition = new Vector2(_ship.Radius, 0f);
             var forwardVector = new Vector2(0f, ThrusterArray.ThrustPower);
 
             _ship.Received().AngularVelocity = 0f;
             _ship.Received(2).ApplyInternalForce(Arg.Any<Force>());
             _ship.Received().ApplyInternalForce(
-                Arg.Is<Force>(x => x.Displacement == leftThrusterPosition && x.Vector == forwardVector));
+                Arg.Is<Force>(x => x.Displacement == _leftThrusterPosition && x.Vector == forwardVector));
             _ship.Received().ApplyInternalForce(
-                Arg.Is<Force>(x => x.Displacement == rightThrusterPosition && x.Vector == forwardVector));
+                Arg.Is<Force>(x => x.Displacement == _rightThrusterPosition && x.Vector == forwardVector));
         }
 
-        //TODO Add tests for rotation
-        //TODO Add tests for when there is less power than requested
-        //TODO Add tests for when no power
+        [Test]
+        public void EngageThrusters_applies_suitable_rotational_forces_when_right_pressed()
+        {
+            _ship.RequestEnergy(0.05f).Returns(0.05f);
+            _thrusterArray.CalculateThrustPattern(ShipAction.TurnRight);
+            _thrusterArray.EngageThrusters();
+
+            var forwardVector = new Vector2(0f, -ThrusterArray.ThrustPower * 0.25f);
+            var backwardVector = new Vector2(0f, ThrusterArray.ThrustPower * 0.25f);
+
+            _ship.Received(2).ApplyInternalForce(Arg.Any<Force>());
+            _ship.Received().ApplyInternalForce(
+                Arg.Is<Force>(x => x.Displacement == _leftThrusterPosition && x.Vector == forwardVector));
+            _ship.Received().ApplyInternalForce(
+                Arg.Is<Force>(x => x.Displacement == _rightThrusterPosition && x.Vector == backwardVector));
+        }
+
+        [Test]
+        public void EngageThrusters_applies_suitable_rotational_forces_when_left_pressed()
+        {
+            _ship.RequestEnergy(0.05f).Returns(0.05f);
+            _thrusterArray.CalculateThrustPattern(ShipAction.TurnLeft);
+            _thrusterArray.EngageThrusters();
+
+            var forwardVector = new Vector2(0f, -ThrusterArray.ThrustPower * 0.25f);
+            var backwardVector = new Vector2(0f, ThrusterArray.ThrustPower * 0.25f);
+
+            _ship.Received(2).ApplyInternalForce(Arg.Any<Force>());
+            _ship.Received().ApplyInternalForce(
+                Arg.Is<Force>(x => x.Displacement == _leftThrusterPosition && x.Vector == backwardVector));
+            _ship.Received().ApplyInternalForce(
+                Arg.Is<Force>(x => x.Displacement == _rightThrusterPosition && x.Vector == forwardVector));
+        }
+
+        [Test]
+        public void EngageThrusters_scales_back_all_thrusters_evenly_when_under_powered()
+        {
+            _ship.RequestEnergy(0.2f).Returns(0.1f);
+            _thrusterArray.CalculateThrustPattern(ShipAction.Thrust);
+            _thrusterArray.EngageThrusters();
+
+            var forwardVector = new Vector2(0f, -ThrusterArray.ThrustPower * 0.5f);
+
+            _ship.Received().AngularVelocity = 0f;
+            _ship.Received(2).ApplyInternalForce(Arg.Any<Force>());
+            _ship.Received().ApplyInternalForce(
+                Arg.Is<Force>(x => x.Displacement == _leftThrusterPosition && x.Vector == forwardVector));
+            _ship.Received().ApplyInternalForce(
+                Arg.Is<Force>(x => x.Displacement == _rightThrusterPosition && x.Vector == forwardVector));
+        }
+
+        [Test]
+        public void EngageThrusters_does_not_thrust_when_no_power_is_available()
+        {
+            _ship.RequestEnergy(0.2f).Returns(0.0f);
+            _thrusterArray.CalculateThrustPattern(ShipAction.ReverseThrust);
+            _thrusterArray.EngageThrusters();
+
+            _ship.Received().AngularVelocity = 0f;
+            _ship.DidNotReceive().ApplyInternalForce(Arg.Any<Force>());
+        }
+        
         //TODO Add tests for thrusting and turning together
 
     }
