57f177acabff79106e62ab4f4d5d3ae7d6a5a161
David Emmerson <david.emmerson@gmail.com>
2012-11-03 21:02:52 +0000

    Updated opencover.bat. Re-run coverage.

--- opencover.bat
+++ opencover.bat
@@ -1,1 +1,2 @@
-C:\Users\Dave\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:"C:\Program Files (x86)\NUnit 2.6\bin\nunit-console-x86.exe" -targetargs:""%1" /noshadow" -filter:+[*]* -output:coverage.xml
+C:\Users\Dave\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:"C:\Program Files (x86)\NUnit 2.6\bin\nunit-console-x86.exe" -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:+[*]* -output:coverage.xml
+pause
