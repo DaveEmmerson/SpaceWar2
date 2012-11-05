aebaef093b6d67f7af875527b7aae94975e9722b
David Emmerson <david.emmerson@gmail.com>
2012-11-05 01:15:33 +0000

    Completed stubs in KeyboardHandlerTests

--- SpaceWar2Tests/Controls/KeyboardHandlerTests.cs
+++ SpaceWar2Tests/Controls/KeyboardHandlerTests.cs
@@ -1,21 +1,48 @@
 ﻿using NUnit.Framework;
 using DEMW.SpaceWar2.Controls;
+using NSubstitute;
+using Microsoft.Xna.Framework.Input;
 
 namespace DEMW.SpaceWar2Tests.Controls
 {
     [TestFixture]
     class KeyboardHandlerTests
     {
-        [Test]
-        public void IsPressed_returns_correct_value()
+        private IKeyboard _keyboard;
+        private KeyboardHandler _keyboardHandler;
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.Fail("Need to be able to mock Keyboard?");
+            _keyboard = Substitute.For<IKeyboard>();
+
+            _keyboard.GetState().Returns(new KeyboardState());
+
+            _keyboardHandler = new KeyboardHandler() { Keyboard = _keyboard };
         }
 
-        [Test]
-        public void IsNewlyPressed_returns_correct_value()
+        [TestCase(Keys.A)]
+        [TestCase(Keys.Up)]
+        [TestCase(Keys.Space)]
+        public void IsPressed_returns_correct_value(Keys key)
         {
-            Assert.Fail("Need to be able to mock Keyboard?");
+            Assert.That(!_keyboardHandler.IsPressed(key));
+            _keyboard.GetState().Returns(new KeyboardState(key));
+            _keyboardHandler.UpdateKeyboardState();
+            Assert.That(_keyboardHandler.IsPressed(key));
+        }
+
+        [TestCase(Keys.B)]
+        [TestCase(Keys.Down)]
+        [TestCase(Keys.Enter)]
+        public void IsNewlyPressed_returns_correct_value(Keys key)
+        {
+            Assert.That(!_keyboardHandler.IsPressed(key));
+            _keyboard.GetState().Returns(new KeyboardState(key));
+            _keyboardHandler.UpdateKeyboardState();
+            Assert.That(_keyboardHandler.IsNewlyPressed(key));
+            _keyboardHandler.UpdateKeyboardState();
+            Assert.That(!_keyboardHandler.IsNewlyPressed(key));
         }
     }
 }
