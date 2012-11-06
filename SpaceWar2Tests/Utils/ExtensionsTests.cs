9b7aca334c6d8b31a43c141a6e5170712be2a94a
David Emmerson <david.emmerson@gmail.com>
2012-11-06 22:27:03 +0000

    Added ExtensionTests

---
+++ SpaceWar2Tests/Utils/ExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using DEMW.SpaceWar2.Utils;
+using System.Collections.Generic;
+using NSubstitute;
+using System;
+
+namespace DEMW.SpaceWar2Tests.Utils
+{
+    class TestBaseClass { }
+    class TestSubClass : TestBaseClass { }
+    class OtherSubClass : TestBaseClass { }
+
+    [TestFixture]
+    class ExtensionsTests
+    {
+        private IList<TestBaseClass> _list;
+        private Action<TestSubClass> _action;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _list = new List<TestBaseClass>();
+            _action = Substitute.For<Action<TestSubClass>>();
+
+        }
+
+        [Test]
+        public void ForEach_runs_without_exception_on_empty_list()
+        {
+            _list.ForEach<TestBaseClass, TestSubClass>(_action);
+            _action.DidNotReceive().Invoke(Arg.Any<TestSubClass>());
+        }
+
+        [Test]
+        public void ForEach_applies_only_to_specified_subtype()
+        {
+            var t1 = new TestSubClass();
+            var t2 = new TestSubClass();
+            _list.Add(new TestBaseClass());
+            _list.Add(new TestBaseClass());
+            _list.Add(t1);
+            _list.Add(new TestBaseClass());
+            _list.Add(new OtherSubClass());
+            _list.Add(new TestBaseClass());
+            _list.Add(t2);
+
+            _list.ForEach<TestBaseClass, TestSubClass>(_action);
+
+            _action.Received(1).Invoke(t1);
+            _action.Received(1).Invoke(t2);
+            _action.DidNotReceive().Invoke(Arg.Is<TestSubClass>((x) => !x.Equals(t1) && !x.Equals(t2))); // Is this the correct way to do this?
+        }            
+
+    }
+}
