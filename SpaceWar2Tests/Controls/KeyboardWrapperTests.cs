a73467d0e7f588daa4f6c8cf03ab557adda77527
Matt Watson <mattwatson1@gmail.com>
2012-12-19 22:35:02 +0000

    Added noddy test for KeyboardWrapper

---
+++ SpaceWar2Tests/Controls/KeyboardWrapperTests.cs
@@ -0,0 +1,21 @@
+﻿using DEMW.SpaceWar2.Controls;
+using Microsoft.Xna.Framework.Input;
+using NUnit.Framework;
+
+namespace DEMW.SpaceWar2Tests.Controls
+{
+    [TestFixture]
+    class KeyboardWrapperTests
+    {
+        //Bit of a noddy test, but at least it causes this code to be executed.
+        [Test]
+        public void IsPressed_returns_correct_value_even_if_repeatedly_called(Keys key)
+        {
+            var _keyboardWrapper = new KeyboardWrapper();
+
+            var state = _keyboardWrapper.GetState();
+
+            Assert.IsNotNull(state);
+        }
+    }
+}
