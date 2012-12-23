a785ca1aaac4222e9db8222faa1b84fcde41253c
Matt Watson <mattwatson1@gmail.com>
2012-12-23 23:19:10 +0000

    Made a few changes to coverage report
    Trying to split it out into two parts, to see the coverage of our
    non-test code, (but keeping the coverage of the test code too because
    that may be of interest too

--- opencover.bat
+++ opencover.bat
@@ -1,2 +1,5 @@
-C:\Users\Dave\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:"C:\Program Files (x86)\NUnit 2.6\bin\nunit-console-x86.exe" -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:+[*]* -output:coverage.xml
+set nunit="C:\Program Files (x86)\NUnit 2.6.2\bin\nunit-console-x86.exe"
+
+C:\Users\%username%\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:%nunit% -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:+[*]* -output:coverageCode.xml
+C:\Users\%username%\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:%nunit% -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:+[*]* -output:coverageTests.xml
 pause
--- reportgen.bat
+++ reportgen.bat
@@ -1,2 +1,3 @@
-"c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:coverage.xml" "-targetdir:.\Coverage Report"
+"c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:coverageCode.xml" "-targetdir:.\Coverage Report\Code"
+"c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:coverageTests.xml" "-targetdir:.\Coverage Report\Tests"
 pause
