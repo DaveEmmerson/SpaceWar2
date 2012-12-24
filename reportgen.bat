535a494cf6de49888af4fe3231a0dde7bcc13cdc
Matt Watson <mattwatson1@gmail.com>
2012-12-24 00:51:55 +0000

    Added new coverage report
    up to 53.9%. Most the easy to test stuff is done =)

--- reportgen.bat
+++ reportgen.bat
@@ -1,3 +1,2 @@
 "c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:coverageCode.xml" "-targetdir:.\Coverage Report\Code"
 "c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:coverageTests.xml" "-targetdir:.\Coverage Report\Tests"
-pause
