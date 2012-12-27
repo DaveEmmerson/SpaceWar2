2130208ca3e66b3196c4132e91b8d31f93e57061
Matt Watson <mattwatson1@gmail.com>
2012-12-27 01:48:06 +0000

    Moved a few coverage files around
    I think we should get rid of all of these files from the repository as
    they change too much and lead to lots of churn.

--- opencover.cmd
+++ opencover.cmd
@@ -1,7 +1,7 @@
 set nunit="C:\Program Files (x86)\NUnit 2.6.2\bin\nunit-console-x86.exe"
 
-C:\Users\%username%\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:%nunit% -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:"+[*]* -[*]*XnaWrappers* -[*SpaceWar2Tests*]*" -output:coverageCode.xml
-C:\Users\%username%\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:%nunit% -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:+[*SpaceWar2Tests*]* -output:coverageTests.xml
+C:\Users\%username%\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:%nunit% -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:"+[*]* -[*]*XnaWrappers* -[*SpaceWar2Tests*]*" -output:".\Coverage Report\coverageCode.xml"
+C:\Users\%username%\AppData\Local\Apps\OpenCover\OpenCover.Console.exe -register:user -target:%nunit% -targetargs:"".\SpaceWar2Tests\bin\Debug\DEMW.SpaceWar2Tests.dll" /noshadow" -filter:+[*SpaceWar2Tests*]* -output:".\Coverage Report\coverageTests.xml"
 
-"c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:coverageCode.xml" "-targetdir:.\Coverage Report\Code"
-"c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:coverageTests.xml" "-targetdir:.\Coverage Report\Tests"
+"c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:.\Coverage Report\coverageCode.xml" "-targetdir:.\Coverage Report\Code"
+"c:\Program Files (x86)\Report Generator\bin\ReportGenerator.exe" "-reports:.\Coverage Report\coverageTests.xml" "-targetdir:.\Coverage Report\Tests"
