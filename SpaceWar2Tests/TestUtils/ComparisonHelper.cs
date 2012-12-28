f66b00cf01523a9aa39a74ea8d476c7c58f9530b
Matt Watson <mattwatson1@gmail.com>
2012-12-28 14:52:46 +0000

    Better comparison methods for Vectors and Forces

--- SpaceWar2Tests/TestUtils/ComparisonHelper.cs
+++ SpaceWar2Tests/TestUtils/ComparisonHelper.cs
@@ -1,21 +1,18 @@
 using System;
 using DEMW.SpaceWar2.Physics;
 using Microsoft.Xna.Framework;
+using NUnit.Framework;
 
 namespace DEMW.SpaceWar2Tests.TestUtils
 {
     static public class ComparisonHelper
     {
-        public static bool Matches(this Force expectedForce, Force actualForce)
+        public static bool Matches(this Force actualForce, Force expectedForce)
         {
-            return Matches(expectedForce.Displacement, actualForce.Displacement) &&
-                   Matches(expectedForce.Vector, actualForce.Vector);
-        }
-
-        public static bool Matches(this Vector2 expectedVector, Vector2 actualVector)
-        {
-            return Matches(expectedVector.X, actualVector.X) &&
-                   Matches(expectedVector.Y, actualVector.Y);
+            return Matches(actualForce.Displacement.X, expectedForce.Displacement.X) &&
+                   Matches(actualForce.Displacement.Y, expectedForce.Displacement.Y) &&
+                   Matches(actualForce.Vector.X, expectedForce.Vector.X) &&
+                   Matches(actualForce.Vector.Y, expectedForce.Vector.Y);
         }
 
         public static bool Matches(this float actual, float expected)
@@ -24,6 +21,18 @@ namespace DEMW.SpaceWar2Tests.TestUtils
 
             bool matchFloat = Math.Abs(expected - actual) < EPSILON;
             return matchFloat;
+        }
+
+        public static void AssertAreEqualWithinTolerance(this Force actualForce, Force expectedForce)
+        {
+            AssertAreEqualWithinTolerance(expectedForce.Displacement, actualForce.Displacement);
+            AssertAreEqualWithinTolerance(expectedForce.Vector, actualForce.Vector);
+        }
+
+        public static void AssertAreEqualWithinTolerance(this Vector2 actualVector, Vector2 expectedVector)
+        {
+            Assert.AreEqual(expectedVector.X, actualVector.X);
+            Assert.AreEqual(expectedVector.Y, actualVector.Y);
         }
     }
 }
