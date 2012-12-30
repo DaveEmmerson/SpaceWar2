1bd5d02d66bcffc2d68098743082b3176f530042
Matt Watson <mattwatson1@gmail.com>
2012-12-30 01:59:03 +0000

    Removed unecessary code from NullArrow and tests

--- SpaceWar2/Graphics/NullArrow.cs
+++ SpaceWar2/Graphics/NullArrow.cs
@@ -1,15 +1,9 @@
 ﻿using DEMW.SpaceWar2.Utils.XnaWrappers;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace DEMW.SpaceWar2.Graphics
 {
     public class NullArrow : IArrow
     {
         public void Draw(IGraphicsDevice graphicsDevice) { }
-        
-        public VertexPositionColor[] Verticies
-        {
-            get { return new VertexPositionColor[0]; }
-        }
     }
 }
