0ae6f12467ed3ab4d3e60ce41f0bb07e974d198e
Matt Watson <mattwatson1@gmail.com>
2012-12-30 14:54:28 +0000

    Completed CameraTests

--- SpaceWar2Tests/Graphics/CameraTests.cs
+++ SpaceWar2Tests/Graphics/CameraTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DEMW.SpaceWar2.Graphics;
+using DEMW.SpaceWar2.Physics;
 using DEMW.SpaceWar2.Utils.XnaWrappers;
 using DEMW.SpaceWar2Tests.TestUtils;
 using Microsoft.Xna.Framework;
@@ -12,38 +13,82 @@ namespace DEMW.SpaceWar2Tests.Graphics
     [TestFixture]
     internal class CameraTests
     {
-        private readonly Color _color = Color.HotPink;
-        private const float Radius = 13f;
+        private Volume _volume;
 
-        private IGraphicsDevice _graphicsDevice;
-        
         [SetUp]
         public void SetUp()
         {
-            _graphicsDevice = Substitute.For<IGraphicsDevice>();
+            _volume = new Volume(-100f, 100f, -200f, 200f, -10f, 10f);
         }
 
         [Test]
-        public void Camera_returns_camera_with_default_target_and_position_projecting_all_of_volume()
+        public void Camera_returns_camera_with_default_view_and_projection_to_cover_all_of_volume()
         {
-            var position = Vector2.Zero;
-            var target = Vector2.Zero;
+            var camera = new Camera(_volume);
 
-            throw new NotImplementedException();
-            //get and test view
-            //get and test projection
+            var actualView = camera.View;
+            var actualProjection = camera.Projection;
+
+            var expectedView = new Matrix(1f, 0f, 0f, 0f,
+                                          0f, 1f, 0f, 0f, 
+                                          0f, 0f, 1f, 0f,
+                                          0f, 0f, -1f, 1f);
+
+            var expectedProjection = new Matrix(0.01f, 0f, 0f, 0f, 
+                                                0f, -0.005f, 0f, 0f, 
+                                                0f, 0f, -0.05f, 0f, 
+                                                0f, 0f, 0.5f, 1f);
+
+            Assert.AreEqual(expectedView, actualView);
+            Assert.AreEqual(expectedProjection, actualProjection);
         }
 
         [Test]
-        public void Pan_updates_View_by_the_specified_Vector()
+        public void Pan_updates_View_by_the_specified_Vector_and_does_not_affect_Projection()
         {
-            throw new NotImplementedException();
+            var camera = new Camera(_volume);
+
+            camera.Pan(new Vector3(1f,2f,4f));
+
+            var actualView = camera.View;
+            var actualProjection = camera.Projection;
+
+            var expectedView = new Matrix(1f, 0f, 0f, 0f,
+                                          0f, 1f, 0f, 0f,
+                                          0f, 0f, 1f, 0f,
+                                          -1f, -2f, -5f, 1f);
+
+            var expectedProjection = new Matrix(0.01f, 0f, 0f, 0f,
+                                                0f, -0.005f, 0f, 0f,
+                                                0f, 0f, -0.05f, 0f,
+                                                0f, 0f, 0.5f, 1f);
+
+            Assert.AreEqual(expectedView, actualView);
+            Assert.AreEqual(expectedProjection, actualProjection);
         }
 
         [Test]
-        public void Zoom_changes_Volume_that_is_visible_causing_the_projection_to_change_accordingly()
+        public void Zoom_changes_Volume_that_is_visible_causing_the_Projection_to_change_accordingly()
         {
-            throw new NotImplementedException();
+            var camera = new Camera(_volume);
+
+            camera.Zoom(10f);
+
+            var actualView = camera.View;
+            var actualProjection = camera.Projection;
+
+            var expectedView = new Matrix(1f, 0f, 0f, 0f,
+                                          0f, 1f, 0f, 0f,
+                                          0f, 0f, 1f, 0f,
+                                          0f, 0f, -1f, 1f);
+
+            var expectedProjection = new Matrix(0.00952381f, 0f, 0f, 0f,
+                                                0f, -0.004761905f, 0f, 0f,
+                                                0f, 0f, -0.05f, 0f,
+                                                0f, 0f, 0.5f, 1f);
+
+            Assert.AreEqual(expectedView, actualView);
+            Assert.AreEqual(expectedProjection, actualProjection);
         }
     }
 }
