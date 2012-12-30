87493ac33baea12b08275271cc2e4c7b0aeea369
Matt Watson <mattwatson1@gmail.com>
2012-12-30 02:16:25 +0000

    Changed Camera properties to fields

--- SpaceWar2/Graphics/Camera.cs
+++ SpaceWar2/Graphics/Camera.cs
@@ -5,44 +5,45 @@ namespace DEMW.SpaceWar2.Graphics
 {
     public class Camera
     {
-        public Vector3 Position { get; set; }
-        public Vector3 Target { get; set; }
-        public IUniverse Universe { get; set; }
+        private Vector3 _position;
+        private Vector3 _target;
+        private readonly IUniverse _universe;
 
         internal static Camera GetDefault(IUniverse universe)
         {
             return new Camera(new Vector3(0, 0, 1), Vector3.Zero, universe);
         }
 
-        public Camera(Vector3 position, Vector3 target, IUniverse universe)
+        private Camera(Vector3 position, Vector3 target, IUniverse universe)
         {
-            Position = position;
-            Target = target;
-            Universe = universe;
+            _position = position;
+            _target = target;
+            _universe = universe;
+
             UpdateProjection();
         }
 
-        public Matrix View { get { return Matrix.CreateLookAt(Position, Target, Vector3.Up); } }
+        public Matrix View { get { return Matrix.CreateLookAt(_position, _target, Vector3.Up); } }
         public Matrix Projection { get; private set; }
 
         public void Pan(Vector3 vector) 
         {
-            Position += vector;
-            Target += vector;
+            _position += vector;
+            _target += vector;
         }
 
         public void Zoom(float amount)
         {
-            Universe.Expand(amount);
+            _universe.Expand(amount);
             UpdateProjection();
         }
 
         private void UpdateProjection()
         {
             Projection = Matrix.CreateOrthographicOffCenter(
-                Universe.MinX, Universe.MaxX,
-                Universe.MaxY, Universe.MinY,
-                Universe.MinZ, Universe.MaxZ
+                _universe.MinX, _universe.MaxX,
+                _universe.MaxY, _universe.MinY,
+                _universe.MinZ, _universe.MaxZ
             );
         }
     }
